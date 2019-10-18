f59febe570152f50e0ef1ab7933179178ca13268
FandyLin <1084957878@qq.com>
2019-10-19 03:04:55 +0800

    首次提交git测试2

--- src/main/java/com/chungke/basic/BasicApplication.java
+++ src/main/java/com/chungke/basic/BasicApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BasicApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(BasicApplication.class, args);
     }
 
